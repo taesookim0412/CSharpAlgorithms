d97712b056d89a0d35962d6f5290f9027e6975be
Tae Soo Kim <taesoo.kim0412@gmail.com>
2021-08-04 17:52:35 -0700

    Initial commit

---
+++ CSharpAlgorithms/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CSharpAlgorithms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            twoSum twoSumObj = new twoSum();
+            int[] twoSumRes = twoSumObj.TwoSum(new int[] {2,7,11,15}, 9);
+            Array.ForEach(twoSumRes, Console.WriteLine);
+        }
+    }
+}
---
+++ CSharpAlgorithms/twoSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpAlgorithms
+{
+    public class twoSum
+    {
+        public int[] TwoSum(int[] nums, int target)
+        {
+
+            Dictionary<int, int> hashMap = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int num = nums[i];
+                int difference = target - num;
+                if (hashMap.ContainsKey(difference))
+                {
+                    return new int[] {hashMap[difference], i};
+                }
+                hashMap.Add(num, i);
+            }
+            return new int[2];
+        }
+    }
+}
